cc1587585e2e32552f6415916b27f04387fe80df
bragaLeandro <rm94513@fiap.com.br>
2023-04-15 14:51:41 -0300

    add: new ProductIsActiveValidator

---
+++ src/main/java/br/com/fiap/validator/implementation/ProductIsActiveValidator.java
@@ -0,0 +1,27 @@
+package br.com.fiap.validator.implementation;
+
+import br.com.fiap.entity.Produto;
+import br.com.fiap.entity.Usuario;
+import br.com.fiap.service.ProdutoService;
+import br.com.fiap.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductIsActiveValidator extends UserValidator {
+
+    private final ProdutoService produtoService;
+
+    @Autowired
+    public ProductIsActiveValidator(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+    @Override
+    public void validate(Usuario user) {
+        for (String productName : user.getProdutos()) {
+            Produto product = produtoService.findByName(productName);
+            if (!product.isHabilitado())
+                throw new IllegalArgumentException("O produto " + product.getNome() + " não está habilitado");
+        }
+    }
+}
