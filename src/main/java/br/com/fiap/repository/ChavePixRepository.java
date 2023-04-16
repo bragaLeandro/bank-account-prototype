1cf987a6f51d679623f193ab3605c98b2748247a
bragaLeandro <rm94513@fiap.com.br>
2023-04-16 17:26:58 -0300

    add: jpaQueries

--- src/main/java/br/com/fiap/repository/ChavePixRepository.java
+++ src/main/java/br/com/fiap/repository/ChavePixRepository.java
@@ -1,9 +1,16 @@
 package br.com.fiap.repository;
 
 import br.com.fiap.entity.ChavePix;
+import br.com.fiap.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ChavePixRepository extends JpaRepository<ChavePix, UUID> {
+
+    List<ChavePix> findChavePixesByUsuario(Usuario usuario);
+    Optional<ChavePix> findChavePixByValorChave(String valorChave);
 }
