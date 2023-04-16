55cabd3884f458bcf4689e8bc894687a7f1ea8ca
bragaLeandro <rm94513@fiap.com.br>
2023-04-16 17:20:37 -0300

    add: fields to transactionDto

--- src/main/java/br/com/fiap/dto/TransacaoPixDto.java
+++ src/main/java/br/com/fiap/dto/TransacaoPixDto.java
@@ -1,4 +1,13 @@
 package br.com.fiap.dto;
 
+import br.com.fiap.entity.ChavePix;
+import lombok.Data;
+
+@Data
 public class TransacaoPixDto {
+
+    private double valorTransferencia;
+    private ChavePix debitor;
+    private ChavePix creditor;
+
 }
