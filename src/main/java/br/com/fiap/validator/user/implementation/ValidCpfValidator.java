594ad70340c7c0bd08ac925ed3db098e0aa47b45
bragaLeandro <rm94513@fiap.com.br>
2023-04-16 17:17:29 -0300

    chg: added todo

--- src/main/java/br/com/fiap/validator/user/implementation/ValidCpfValidator.java
+++ src/main/java/br/com/fiap/validator/user/implementation/ValidCpfValidator.java
@@ -1,7 +1,7 @@
-package br.com.fiap.validator.implementation;
+package br.com.fiap.validator.user.implementation;
 
 import br.com.fiap.entity.Usuario;
-import br.com.fiap.validator.UserValidator;
+import br.com.fiap.validator.user.UserValidator;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
@@ -10,11 +10,14 @@ import java.util.regex.Pattern;
 public class ValidCpfValidator extends UserValidator {
 
     private static final String CPF_REGEX = "^(\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}|\\d{11})$";
+
     @Override
     public void validate(Usuario user) {
         if (!hasValidCpf(user.getCpf()))
             throw new IllegalArgumentException("CPF inválido");
     }
+
+    //TODO: Change to correct validator just like pixCpfValidator
     public static boolean hasValidCpf(String cpf) {
         return Pattern.compile(CPF_REGEX).matcher(cpf).matches();
     }
