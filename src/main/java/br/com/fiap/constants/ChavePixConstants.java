5a5b9f8547151928e14fce964c5be5c0edc3fc5b
bragaLeandro <rm94513@fiap.com.br>
2023-04-16 17:25:47 -0300

    add: pixKeyConstants

---
+++ src/main/java/br/com/fiap/constants/ChavePixConstants.java
@@ -0,0 +1,9 @@
+package br.com.fiap.constants;
+
+public class ChavePixConstants {
+
+    public static final String EVP = "EVP";
+    public static final String CPF = "CPF";
+    public static final String TELEFONE = "TELEFONE";
+    public static final String EMAIL = "EMAIL";
+}
