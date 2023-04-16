b318b66e0c70368e9e83972005c4c36183ceec90
bragaLeandro <rm94513@fiap.com.br>
2023-04-16 17:15:38 -0300

    add: method removeProductFromUser

--- src/main/java/br/com/fiap/service/UsuarioService.java
+++ src/main/java/br/com/fiap/service/UsuarioService.java
@@ -1,14 +1,14 @@
 package br.com.fiap.service;
 
+import br.com.fiap.entity.Produto;
 import br.com.fiap.entity.Saldo;
 import br.com.fiap.entity.Usuario;
 import br.com.fiap.repository.UsuarioRepository;
-import br.com.fiap.validator.UserValidator;
+import br.com.fiap.validator.user.UserValidator;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -60,6 +60,19 @@ public class UsuarioService {
         usuarioRepository.save(user);
     }
 
+    public void removeProductFromUser(UUID userId, UUID productId) {
+        Usuario usuario = this.findById(userId);
+
+        Produto produto = usuario.getProdutos().stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado na lista de produtos do usuário"));
+
+        usuario.getProdutos().remove(produto);
+
+        usuarioRepository.save(usuario);
+    }
+
     public Usuario findById(UUID uuid) {
         return usuarioRepository.findById(uuid)
                 .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
@@ -68,7 +81,4 @@ public class UsuarioService {
     public List<Usuario> findAllUsers() {
         return usuarioRepository.findAll();
     }
-
-
-
 }
