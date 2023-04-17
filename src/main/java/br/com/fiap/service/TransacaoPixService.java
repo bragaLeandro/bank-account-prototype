277e4885e089088c9dcacbebe8389bdd742125d7
bragaLeandro <rm94513@fiap.com.br>
2023-04-17 01:17:22 -0300

    chg: register of transaction

--- src/main/java/br/com/fiap/service/TransacaoPixService.java
+++ src/main/java/br/com/fiap/service/TransacaoPixService.java
@@ -2,21 +2,28 @@ package br.com.fiap.service;
 
 import br.com.fiap.dto.TransacaoPixDto;
 import br.com.fiap.entity.Saldo;
+import br.com.fiap.entity.TransacaoPix;
 import br.com.fiap.entity.Usuario;
 import br.com.fiap.repository.ChavePixRepository;
 import br.com.fiap.repository.SaldoRepository;
+import br.com.fiap.repository.TransacaoPixRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class TransacaoPixService {
 
     private final SaldoRepository saldoRepository;
     private final ChavePixRepository chavePixRepository;
+    private final TransacaoPixRepository transacaoPixRepository;
 
-    public TransacaoPixService(SaldoRepository saldoRepository, ChavePixRepository chavePixRepository) {
+    public TransacaoPixService(SaldoRepository saldoRepository, ChavePixRepository chavePixRepository, TransacaoPixRepository transacaoPixRepository) {
         this.saldoRepository = saldoRepository;
         this.chavePixRepository = chavePixRepository;
+        this.transacaoPixRepository = transacaoPixRepository;
     }
 
     @Transactional
@@ -42,7 +49,11 @@ public class TransacaoPixService {
         saldoDebitor.setValor(saldoDebitor.getValor() - transacaoPixDto.getValorTransferencia());
         saldoCreditor.setValor(saldoCreditor.getValor() + transacaoPixDto.getValorTransferencia());
 
-        saldoRepository.save(saldoDebitor);
-        saldoRepository.save(saldoCreditor);
+        TransacaoPix transacaoPix = new TransacaoPix(usuarioDebitor, usuarioCreditor, "Success", transacaoPixDto.getValorTransferencia());
+
+        List<Saldo> modifiedBalances = Arrays.asList(saldoDebitor, saldoCreditor);
+
+        transacaoPixRepository.save(transacaoPix);
+        saldoRepository.saveAll(modifiedBalances);
     }
 }
