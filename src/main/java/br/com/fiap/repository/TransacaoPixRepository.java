da7eca1a11450cf60a994779102b849f0f38c9f1
bragaLeandro <rm94513@fiap.com.br>
2023-04-23 23:29:37 -0300

    add: jpa queries

--- src/main/java/br/com/fiap/repository/TransacaoPixRepository.java
+++ src/main/java/br/com/fiap/repository/TransacaoPixRepository.java
@@ -1,9 +1,15 @@
 package br.com.fiap.repository;
 
 import br.com.fiap.entity.TransacaoPix;
+import br.com.fiap.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransacaoPixRepository extends JpaRepository<TransacaoPix, UUID> {
+
+    List<TransacaoPix> findTransacaoPixesByCreditor(Usuario user);
+    List<TransacaoPix> findTransacaoPixesByDebitor(Usuario user);
+
 }
